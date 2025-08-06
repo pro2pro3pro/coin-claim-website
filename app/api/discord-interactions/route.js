422b83b87f0416e25fd21fbf151cc576c687b7a1
Tên GitHub của bạn <Email GitHub ca bn>
2025-08-06 16:10:27 +0700

    add discord interactions endpoint

--- app/api/discord-interactions/route.js
+++ app/api/discord-interactions/route.js
@@ -10,7 +10,7 @@ export async function POST(req) {
     rawBody,
     signature,
     timestamp,
-    process.env.DISCORD_PUBLIC_KEY
+    process.env.DISCORD_PUBLIC_KEY=82d8d2a2cc0a52ba843a7daee05c79a3cc1396df7da1ced5d0b2e7bb06b63848
   );
 
   if (!isValid) return NextResponse.json('Invalid request', { status: 401 });
@@ -18,11 +18,10 @@ export async function POST(req) {
   const interaction = JSON.parse(rawBody);
 
   if (interaction.type === 1) {
-    // Discord xác thực (PING)
+    // Đây là xác minh ban đầu của Discord (PING)
     return NextResponse.json({ type: 1 });
   }
 
-  // Xử lý slash command ở đây
-
+  // Nếu slash command được gọi, trả lời lại ở đây
   return NextResponse.json({ type: 4, data: { content: 'Hello từ bot!' } });
 }
